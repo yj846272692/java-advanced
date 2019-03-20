f0326a985fa10147bc91299d4d01c406244d70cc
yangj <846272692@qq.com>
2019-03-21 07:55:32 +0800

    枚举类的练习

---
+++ collection-study/src/com/soft1841/Poker.java
@@ -0,0 +1,147 @@
+package com.soft1841;
+/**
+ * 集合类的练习
+ * @author 杨晶 组
+ * 2019.03.20
+ */
+
+    public class Poker implements Comparable<Poker> {
+        private String point;
+        private String color;
+
+        public Poker(String point, String color) {
+            this.point = point;
+            this.color = color;
+        }
+
+        public String getPoint() {
+            return point;
+        }
+
+        public void setPoint(String point) {
+            this.point = point;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((color == null) ? 0 : color.hashCode());
+            result = prime * result + ((point == null) ? 0 : point.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Poker other = (Poker) obj;
+            if (color == null) {
+                if (other.color != null)
+                    return false;
+            } else if (!color.equals(other.color))
+                return false;
+            if (point == null) {
+                if (other.point != null)
+                    return false;
+            } else if (!point.equals(other.point))
+                return false;
+            return true;
+        }
+
+        /*
+         * 先比较点数在比较花色，如果点数不同就不用比较花色了。
+         */
+        @Override
+        public int compareTo(Poker o) {
+
+            /*
+             * 先比较点数
+             */
+            int num = 0;
+            if (this.PointTransformatio() > o.PointTransformatio()) {
+                num = 1;
+            }
+            // 如果点数相等，再比较花色
+            else if (this.PointTransformatio() == o.PointTransformatio()) {
+                if (this.ColorTransformatio() > o.ColorTransformatio()) {
+                    num = 1;
+                }
+                if (this.ColorTransformatio() == o.ColorTransformatio()) {
+                    num = 0;
+                }
+                if (this.ColorTransformatio() < o.ColorTransformatio()) {
+                    num = -1;
+                }
+            } else
+                num = -1;
+            return num;
+
+        }
+
+        /*
+         * 写一个方法把 J、Q、K、A转化为int类型的11 12 13 14 2——10转化为int类型的2——10
+         */
+        public int PointTransformatio() {
+            String c = this.point;
+
+            int it;
+            switch (c) {
+                case "J":
+                    it = 11;
+                    break;
+                case "Q":
+                    it = 12;
+                    break;
+                case "K":
+                    it = 13;
+                    break;
+                case "A":
+                    it = 14;
+                    break;
+                default:
+                    it = Integer.parseInt(c);
+            }
+            return it;
+        }
+        /*
+         * 写一个方法把黑桃、红桃、梅花、方块转换为int类型的1-4
+         */
+
+        public int ColorTransformatio() {
+            String s = this.color;
+            int it = 0;
+            switch (s) {
+                case "黑桃":
+                    it = 1;
+                    break;
+                case "红桃":
+                    it = 2;
+                    break;
+                case "梅花":
+                    it = 3;
+                    break;
+                case "方块":
+                    it = 4;
+                    break;
+
+            }
+
+            return it;
+        }
+
+
+    }
+
---
+++ collection-study/src/com/soft1841/PokerSetTest.java
@@ -0,0 +1,152 @@
+package com.soft1841;
+/**
+ * 集合类的练习
+ * @author 杨晶 组
+ * 2019.03.20
+ */
+
+import java.util.*;
+
+public class PokerSetTest {
+    private List<Poker> hs;// 用来装一副牌的
+    private TreeSet<Poker> ts1;// 玩家一用来装牌的
+    private TreeSet<Poker> ts2;// 玩家二用来装牌的
+    private int i[] = new int[52];
+
+    public PokerSetTest() {
+        this.hs = new ArrayList<Poker>();
+        this.ts1 = new TreeSet<Poker>();
+        this.ts2 = new TreeSet<Poker>();
+    }
+
+    /*
+     * 添加牌
+     */
+    public void addpoker() {
+
+        String s1[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        String s2[] = { "黑桃", "红桃", "梅花", "方块" };
+        for (int i = 0; i < s1.length; i++)
+            for (int j = 0; j < s2.length; j++) {
+                hs.add(new Poker(s1[i], s2[j]));
+            }
+
+    }
+
+
+    /*
+     * 利用Iterator访问
+     */
+    public void testIterator(Collection<Poker> hs) {
+        Iterator it = hs.iterator();
+        int size = hs.size();
+
+        System.out.println("取出的牌如下：");
+
+
+        while (it.hasNext()) {
+            Poker po = (Poker) it.next();
+            System.out.print(po.getColor()+" " + po.getPoint());
+        }
+        System.out.println();
+        System.out.println(" ");
+    }
+
+    /*
+     * 打乱list里面元素的数据 利用随机数，取出0-51中52个不同的随机数 返回数组
+     */
+    public static int[] randomCommon(int min, int max, int n) {
+        if (n > (max - min) || max < min) {
+            return null;
+        }
+        int[] result = new int[n];
+        int count = 0;
+        while (count < n) {
+            int num = (int) (Math.random() * (max - min));
+            boolean flag = true;
+            for (int j = 0; j <= count; j++) {
+                if (num == result[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                result[count] = num;
+                count++;
+            }
+
+            // 到最后一个的时候退出语句
+            if (count == n - 1) {
+                break;
+            }
+
+        }
+        return result;
+    }
+
+    /*
+     * 打乱牌 每一张牌和随机比这张下标大的牌互相交换
+     */
+    public void UpsetCards() {
+
+        int in[];
+        int wo;
+        for (int i = 0; i < hs.size(); i++) {
+
+            Poker p1 = hs.get(i);
+            do {
+                wo = (int) (Math.random() * 51);
+            } while (wo > i);
+            Poker p2 = hs.get(wo);
+            hs.set(i, p2);
+            hs.set(wo, p1);
+            i++;
+        }
+    }
+
+    /*
+     * 1.发牌，每人每次只发两张牌，轮流发 2.比较大小
+     */
+    public void givepoker() {
+        // 每次发牌都刷新
+        UpsetCards();
+        testIterator(hs);
+        for (int i = 0; i < 4; i++) {
+            ts1.add(hs.get(i));
+            ts2.add(hs.get(++i));
+
+        }
+        System.out.println("给玩家一发牌：");
+
+        testIterator(ts1);
+        System.out.println(" ");
+        System.out.println("给玩家二发牌：");
+
+        testIterator(ts2);
+        System.out.println(" ");
+        /*
+         * 比较大小
+         */
+        if(ts1.last().compareTo(ts2.last())>0)
+        {
+            System.out.println("恭喜玩家一赢了本次比赛");
+        }
+        else
+        {
+            System.out.println("恭喜玩家二赢了本次比赛");
+        }
+        // 发完牌了清空
+        ts1.clear();
+        ts2.clear();
+    }
+    public static void main(String[] args) {
+
+        PokerSetTest po = new PokerSetTest();
+        po.addpoker();
+        po.givepoker();
+
+    }
+
+
+
+}
